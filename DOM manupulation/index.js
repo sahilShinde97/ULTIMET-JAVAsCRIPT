164843ab3dbb47f7eb2839a3785c37b80de1f11f
Sahil Shinde <sahilshinde9730@gmail.com>
2024-01-19 01:08:50 +0530

    tip Calculator project

--- DOM manupulation/index.js
+++ DOM manupulation/index.js
@@ -4,8 +4,9 @@ let redDiv = document.getElementById('red')
 let yellowDiv = document.getElementById('yellow')
 let greenDiv = document.getElementById('green')
 
+/*
 redDiv.onclick = () => {
-  console.log('user chose : Rock')
+  console.log('red')
   // getComputerChoice => 'paper'
   // userChoice vs computerChoice => result
   // -1 , 0 , 1
@@ -14,8 +15,30 @@ redDiv.onclick = () => {
 
 yellowDiv.onclick = () => console.log('Yellow')
 greenDiv.onclick = () => console.log('Green')
-
+*/
 const squares = document.querySelectorAll('.colorSquare')
-console.log(squares)
+//console.log(squares)
 
-console.log(squares[0].value)
+//console.log(squares[0].value)
+
+// forEach loop 
+const timesClicked = {'red': 0, 'yellow': 0 ,'green' : 0,}
+squares.forEach(square => {
+  square.onclick = () =>{
+    timesClicked[square.value] += 1;
+    square.innerText = timesClicked[square.value]
+    
+  }
+})
+
+function clearScores(){
+  timesClicked.red = 0;
+  timesClicked.yellow = 0;
+  timesClicked.green = 0;
+  squares.forEach(square => {
+    square.innerText = '';
+  })
+}
+
+const clearGameBtn = document.getElementById('clear-game')
+clearGameBtn.onclick = () => clearScores()
